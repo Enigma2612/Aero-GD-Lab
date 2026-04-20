e07b4bee97de49c2244d05d8c8b63b11d03b13db
Shaurya Gupta <shauryagupta2612@gmail.com>
2026-04-20 23:19:47 +0530

    exp 8 continued

--- Exp 8/code.py
+++ Exp 8/code.py
@@ -1,14 +1,22 @@
 import csv
+psi_to_bar = lambda p : p * 0.0689476
+abs_p = lambda p : p + P_atm
 
-P01 = ...
-P02 = ...
+P01 = abs_p(psi_to_bar(-0.5))
+P02 = abs_p(psi_to_bar(-2.38))
 P_atm = 1.01325
 
-P_2_M1 = []
-P_neg_2_M1 = []
 P_2_M2 = []
 P_neg_2_M2 = []
 
-psi_to_bar = lambda p : p * 0.0689476
-abs_p = lambda p : p + P_atm
+m1_free = []
+
+
+
+with open('Data/5psi/-2_M1.csv') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        m1_free.append(row[1])
+        needed = row[2:4] + row[5:6] + row[6:]
 
