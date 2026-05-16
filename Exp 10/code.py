fa95c32fc210e6d6f5cd8b3fd2d67e997d4a172c
Shaurya Gupta <shauryagupta2612@gmail.com>
2026-05-16 18:09:30 +0530

    added critical mach number

--- Exp 10/code.py
+++ Exp 10/code.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import csv
+import numpy as np
 
 psi20 = [-3.9,-5.0,-5.0,-4.6,-3.9,-3.6,-3.4,-3.2,-3.0]
 psi25 = [-4.6,-6.1,-6.1,-5.5,-4.6,-4.3,-4.0,-3.8,-3.6]
@@ -17,7 +17,8 @@ conv = lambda p: abs_p(psi_to_bar(p))
 cp = lambda p, p_inf, m : 2/(1.4 * m**2) * (p/p_inf - 1) if not isinstance(p, list) else [cp(i,p_inf,m) for i in p]
 P_atm = psi_to_bar(14.6)
 M = lambda P: ((2 / (gamma - 1)) * (((P0 / P) ** ((gamma - 1) / gamma)) - 1)) ** 0.5 if not isinstance(P, list) else [M(i) for i in P]
-
+pratio = lambda m: (1 + (gamma-1)/2 * m**2)**(-(gamma)/(gamma-1)) if not isinstance(m, list) else [pratio(i) for i in m]
+pg = lambda cp,m: cp/(abs(1-m**2))**0.5 if not isinstance(m, list) else [pg(cp,i) for i in m]
 P0 = P_atm
 print(P0)
 
@@ -66,5 +67,25 @@ plt.xlabel("x/c Position on Airfoil")
 plt.ylabel("Local Mach Number")
 plt.title("Local Mach Number Variation Along NACA0012 Airfoil")
 plt.savefig(fname='mach_variation', dpi=600, bbox_inches='tight')
+plt.show()
+
+#GRAPH 3 : P/P0 VS M
+x1 = list(np.linspace(0,2,300))
+x2 = list(np.linspace(0,0.9,300))
+
+if 1 in x1: x1.remove(1)
+y1 = pratio(x1)
+Cp_inc = -0.414
+y2 = pg(Cp_inc, x2)
+
+plt.figure(figsize=(10,6))
+plt.xlabel("Mach Number")
+plt.plot(x1,y1, label='Pressure Ratio P/P0')
+plt.plot(x2,np.abs(y2), label='Compressible CP')
+plt.axvline(x=0.783761, ymax=(0.6), linestyle='--', color='green')
+plt.scatter([0.783761], [0.6666], color='green', marker='o', label=f'Critical Mach Number\nMcr = 0.783761')
+plt.legend(frameon=True)
+plt.title("Theoretical Critical Mach Number Estimation")
+plt.savefig(fname='critical_mach', dpi=600, bbox_inches='tight')
 plt.show()
 
